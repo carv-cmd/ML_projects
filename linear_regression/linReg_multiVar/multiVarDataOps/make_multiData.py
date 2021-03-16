e7b8fefe5da7c829d943a0bf26e893cb508b1fc7
carv-cmd <bencarv1324@gmail.com>
2021-03-16 00:21:43 -0400

    Fix GenData returning wrong object instance

--- linear_regression/linReg_multiVar/multiVarDataOps/make_multiData.py
+++ linear_regression/linReg_multiVar/multiVarDataOps/make_multiData.py
@@ -1,15 +1,21 @@
-import logging as logs
 from sklearn.datasets import make_regression as gen_reg
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 ####################################################
 ####################################################
 class GenData:
+    """
+    Generates random multivariate test sets for multivariate linear regression models
+    Use singular 'get' methods to access default set size, number of features, and the Gaussian noise
+    Use singular 'set' methods to customize data set attributes
+    Calls 'set_all' method to set all parameters at once
+    Calls "make_df" method to create pd.DataFrame with given attributes
+    """
 
-    def __init__(self, n=25, features=1, noisy=25):
-        self.dps = pd.DataFrame
+    def __init__(self, n=25, features=2, noisy=25):
+        self.df = pd.DataFrame()
         self.n = n
         self.features = features
         self.noisy = noisy
@@ -32,31 +38,35 @@ class GenData:
     def set_noise(self, user_noise=10):
         self.noisy = user_noise
 
+    def set_all(self, user_n=15, user_feats=2, user_noise=10):
+        user_n = int(input("Enter the desired set size: "))
+        user_feats = int(input("Enter the desired number of features: "))
+        user_noise = int(input("Enter the desired noise: "))
+        self.set_n(user_n)
+        self.set_features(user_feats)
+        self.set_noise(user_noise)
+
+    def make_df(self):
+        features, y = gen_reg(n_samples=self.get_n(), n_features=self.get_features(), noise=self.get_noise())
+        formed = np.concatenate((features, y.reshape(self.get_n(), 1)), axis=1)
+        self.df = pd.DataFrame(formed)
+
     def __str__(self):
         return f'\nSetSize:({self.n}) :: ' \
                f'NumFeatures:({self.features}) :: ' \
-               f'Noise:({self.noisy})'
+               f'Noise:({self.noisy})' \
+               f'\ndf.shape: {self.df.shape}, ' \
+               f'df.size: {self.df.size}, ' \
+               f'df.ndim: {self.df.ndim}\n' \
+               f'\n{self.df}'
 
 
 def tester_set():
     dp = GenData()
-    dp.set_n(25)
-    dp.set_features(2)
-    dp.set_noise(15)
-
-    print(dp.__str__())
+    dp.set_all()
+    dp.make_df()
+    return dp
 
 
-tester_set()
-
-# dati = 150
-# feats = 6
-#
-# features, y = reg_func(n_samples=dati, n_features=feats, noise=25)
-# formed = np.concatenate((features, y.reshape(dati, 1)), axis=1)
-# df = pd.DataFrame(formed)
-#
-# print(f'\nDataFrame_TestSet: '
-#       f'\nndim={df.ndim}, shape={df.shape}, size={df.size}\n'
-#       f'\n{df}')
-
+dataSet = tester_set()
+print(type(dataSet))
